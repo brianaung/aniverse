9178bea6a7dabd8bdfc3ac9ccd179d10173ad918
Brian Aung <soekhantaung16@gmail.com>
2022-11-14 23:13:01 +1100

    fetch search anime data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { getAllTopAnime } from '../lib/anime'
+import { getAllTopAnime, getAnimeSearch } from '../lib/anime'
 import Player from '../components/player'
 import Layout from '../components/layout'
 
 export default function Home({
-  allTopAnime,
+  queryResults,
 }: {
-  allTopAnime: {
+  queryResults: {
     id: string
     title: string
     image: string
-    url: string
-    genres: [string]
+    releaseDate: string
+    subOrDub: string
   }[]
 }) {
   return (
@@ -27,9 +27,9 @@ export default function Home({
         <Player src="https://wwwx16.gogocdn.stream/videos/hls/3wT6FzERnZZ6t_9FXeYgbA/1668426010/17075/bcc369738325d609c828bc6c16f9a7fd/ep.1.1657689068.720.m3u8" />
       </section>
       <section>
-        <h2>Popular</h2>
+        <h2>Testing: data fetching</h2>
         <ul>
-          {allTopAnime.map((anime) => (
+          {queryResults.map((anime) => (
             <li key={anime.id}>
               {anime.title}
               <Image src={anime.image} height={180} width={180} alt={anime.title} />
@@ -42,16 +42,21 @@ export default function Home({
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allTopAnime = await getAllTopAnime()
+  // todo: testing search function (might need to use client side rendering)
+  const queryResults = await getAnimeSearch()
+  console.log(queryResults)
+
   // error handling
-  if (typeof allTopAnime === 'undefined') {
+  if (typeof queryResults === 'undefined') {
     return {
       notFound: true,
     }
   }
+
   return {
     props: {
-      allTopAnime,
+      queryResults,
     },
   }
 }
+
