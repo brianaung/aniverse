156b98d14ab1d0ad73e341eea929cc89904cbb79
Brian Aung <soekhantaung16@gmail.com>
2022-11-29 19:16:20 +1100

    fix image in info page and add trailer video

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,14 @@
 import styles from './navbar.module.scss'
-import utilStyles from '../styles/utils.module.scss'
+// import utilStyles from '../styles/utils.module.scss'
 import Link from 'next/link'
 import Searchbar from './searchbar'
 
 export default function Navbar() {
   return (
     <div className={styles.container}>
-      {/* back to home */}
-      <Link className={utilStyles.mainHeader} href="/">
-        {/*どやなル*/}
-        アニメ
-      </Link>
 
-      {/* getAllTopAnime */}
-      {/* getAllRecentAnime */}
       <div className={styles.navSelections}>
+        <Link href="/">Home</Link>
         <Link href="/anime/popular">Popular</Link>
         <Link href="/anime/trending">Trending</Link>
       </div>
--- src/pages/anime/info/[id].tsx
+++ src/pages/anime/info/[id].tsx
@@ -7,6 +7,23 @@ import Image from 'next/image'
 // import {PlayCircle} from '@styled-icons/bootstrap/PlayCircle'
 import { PlayCircle } from '@styled-icons/material-twotone/PlayCircle'
 
+/* taken from stack overflow */
+function pickContrastColor(bgColor: string, lightColor: string, darkColor: string) {
+  var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+  var r = parseInt(color.substring(0, 2), 16); // hexToR
+  var g = parseInt(color.substring(2, 4), 16); // hexToG
+  var b = parseInt(color.substring(4, 6), 16); // hexToB
+  var uicolors = [r / 255, g / 255, b / 255];
+  var c = uicolors.map((col) => {
+    if (col <= 0.03928) {
+      return col / 12.92;
+    }
+    return Math.pow((col + 0.055) / 1.055, 2.4);
+  });
+  var L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
+  return (L > 0.179) ? darkColor : lightColor;
+}
+
 const fetcher: Fetcher<AnimeInfo, string> = (arg: string) => fetch(arg).then((res) => res.json())
 
 export default function AnimeInfoPage() {
@@ -32,19 +49,31 @@ export default function AnimeInfoPage() {
       {data && !error && (
         <>
           {/* display anime meta data */}
-          <Image src={data.cover} width={240} height={80} layout='responsive' alt={data.title.userPreferred} />
-          <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}>
-            <h1>{data.title.english} [{data.title.native}]</h1>
-            <h3>{data.releaseDate} ({data.status}) | <span style={{fontSize:'18px', fontWeight:'normal'}}>{data.totalEpisodes} episodes</span></h3>
+          <div style={{width:'100%', height:'auto', maxHeight:'500px', overflow:'hidden'}}>
+            <Image style={{width:'100%', height:'auto'}} quality="100" src={data.cover} width={10000} height={100000} alt={data.title.english} />
           </div>
-          {/* todo: style genres into tags */}
-          <div style={{display:'flex',gap:'.5rem'}}>{data.genres.map(genre => <p>{genre}</p>)}</div>
+          <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+            <h1>{data.title.english}</h1>
+            <h4>{data.releaseDate} ({data.status}) | {data.totalEpisodes} episodes</h4>
+          </div>
+          {/* todo: seperate genres into tags components */}
+          <div style={{display:'flex',gap:'.5rem'}}>
+            {data.genres.map(genre => 
+              <div style={{
+                backgroundColor:`${data.color}`,
+                // filter: 'invert(100%)',
+                color: pickContrastColor(`${data.color}`, 'white', 'black'),
+                borderRadius:'9999px',
+                border:'solid 1px',
+                borderColor: pickContrastColor(`${data.color}`, 'white', 'black'),
+                padding:'0 .5rem'}}>
+                {genre}
+              </div>
+            )}
+          </div>
+          <iframe style={{all:'unset', width:'700px', height:'500px'}} src={`https://www.youtube.com/embed/${data.trailer.id}?controls=0&disablekb=1&mute=1&autoplay=1&playlist=${data.trailer.id}&loop=1&cc_load_policy=0&iv_load_policy=3&modestbranding=0&rel=0`} />
 
-          <p dangerouslySetInnerHTML={{ __html: data.description }} />
+          <p style={{fontStyle:'italic'}} dangerouslySetInnerHTML={{ __html: data.description }} />
 
           {/* return a list of episodes */}
           <div className={utilStyles.btnListContainer}>
@@ -52,7 +81,7 @@ export default function AnimeInfoPage() {
               <div className={utilStyles.playBtn} key={ep.id} onClick={() => handlePlay(ep, index)}>
                 {/* episode image with play button icon */}
                 <div style={{position:'relative'}}>
-                  <Image src={ep.image} width={200} height={100} alt={ep.title} />
+                  <Image src={ep.image} width={250} height={150} alt={ep.title} />
                   <PlayCircle style={{position:'absolute', top:'50%', left:'50%', marginLeft:'-25px', marginTop:'-25px'}} color='black' width={50} height={50} />
                 </div>
                 {/* ----------------------------------- */}
