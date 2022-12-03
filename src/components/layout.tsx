8fd42533e795de0cbe98dfd5754d8f7968b16755
Brian Aung <soekhantaung16@gmail.com>
2022-12-03 16:38:32 +1100

    add footer to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,6 @@
+import { Text } from '@chakra-ui/react'
 import Head from 'next/head'
+import Link from 'next/link'
 import React from 'react'
 import styles from './layout.module.scss'
 import Navbar from './navbar'
@@ -10,7 +12,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <link rel="icon" href="/makima.ico" />
       </Head>
       <Navbar />
-      <main>{children}</main>
+      <main style={{minHeight:"90vh"}}>{children}</main>
+      <footer style={{marginTop: 'auto'}}><Text fontSize='sm' as='cite'>created by <Link href='https://github.com/brianaung'>@brianaung</Link></Text></footer>
     </div>
   )
 }
