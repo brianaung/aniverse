beed56a1689ac75c8ddcd8411197168f2fd309ee
Brian Aung <soekhantaung16@gmail.com>
2022-12-04 17:59:39 +1100

    enable inter font

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,16 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
-const config: ThemeConfig = {
+const colorConfig: ThemeConfig = {
   initialColorMode: 'light',
   useSystemColorMode: false,
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme({ 
+  colorConfig,
+  fonts: {
+    heading: `'Inter', -apple-system, BlinkMacSystemFont, SegoeUI, Roboto, sans-serif`,
+    body: `'Inter', -apple-system, BlinkMacSystemFont, SegoeUI, Roboto, sans-serif`,
+  }
+})
 
 export default theme
