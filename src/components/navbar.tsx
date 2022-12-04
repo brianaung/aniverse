f1628b617bb47ec069541d8f6c78789291a8a828
Brian Aung <soekhantaung16@gmail.com>
2022-12-04 17:49:11 +1100

    add colormode toggle

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,17 @@
+import { Button, useColorMode } from '@chakra-ui/react'
 import styles from './navbar.module.scss'
 import Searchbar from './searchbar'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 
 export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
     <div className={styles.container}>
       <Searchbar />
+      <Button onClick={toggleColorMode}>
+        {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+      </Button>
     </div>
   )
 }
