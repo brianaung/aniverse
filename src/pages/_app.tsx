bc7f8bf6dc0b705dfd7799fc753247da6014cc39
Brian Aung <soekhantaung16@gmail.com>
2022-12-04 16:42:47 +1100

    add colormode config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
+import theme from '../styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
       <Analytics />
     </ChakraProvider>
