759d63c2b98605d6f8e2a736220c69baccb5e4d7
Brian Aung <soekhantaung16@gmail.com>
2022-12-09 02:27:56 +1100

    clean

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,31 +2,31 @@ import { ChakraProvider } from '@chakra-ui/react'
 import '@fontsource/raleway/700.css'
 import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
+import Router from 'next/router'
+import { useEffect, useState } from 'react'
 import '../styles/globals.scss'
 import theme from '../styles/theme'
-import Router from 'next/router'
-import React, { useState, useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(false);
-    useEffect(() => {
-      const start = () => {
-        console.log("start");
-        setLoading(true);
-      };
-      const end = () => {
-        console.log("finished");
-        setLoading(false);
-      };
-      Router.events.on("routeChangeStart", start);
-      Router.events.on("routeChangeComplete", end);
-      Router.events.on("routeChangeError", end);
-      return () => {
-        Router.events.off("routeChangeStart", start);
-        Router.events.off("routeChangeComplete", end);
-        Router.events.off("routeChangeError", end);
-      };
-    }, []);
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const start = () => {
+      console.log('start')
+      setLoading(true)
+    }
+    const end = () => {
+      console.log('finished')
+      setLoading(false)
+    }
+    Router.events.on('routeChangeStart', start)
+    Router.events.on('routeChangeComplete', end)
+    Router.events.on('routeChangeError', end)
+    return () => {
+      Router.events.off('routeChangeStart', start)
+      Router.events.off('routeChangeComplete', end)
+      Router.events.off('routeChangeError', end)
+    }
+  }, [])
   return (
     <ChakraProvider theme={theme}>
       {loading ? (
