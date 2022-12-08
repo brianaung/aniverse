e07f3fbb419a5e55e01158cb53d534c22a06cc52
Brian Aung <soekhantaung16@gmail.com>
2022-12-09 02:16:13 +1100

    add loading state while routing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,39 @@ import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
 import theme from '../styles/theme'
+import Router from 'next/router'
+import React, { useState, useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false);
+    useEffect(() => {
+      const start = () => {
+        console.log("start");
+        setLoading(true);
+      };
+      const end = () => {
+        console.log("finished");
+        setLoading(false);
+      };
+      Router.events.on("routeChangeStart", start);
+      Router.events.on("routeChangeComplete", end);
+      Router.events.on("routeChangeError", end);
+      return () => {
+        Router.events.off("routeChangeStart", start);
+        Router.events.off("routeChangeComplete", end);
+        Router.events.off("routeChangeError", end);
+      };
+    }, []);
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-      <Analytics />
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <Component {...pageProps} />
+          <Analytics />
+        </>
+      )}
     </ChakraProvider>
   )
 }
