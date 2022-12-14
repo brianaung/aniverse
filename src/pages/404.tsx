cbdc97256cb6d3a368b44bf22e3c6323e85b57d2
Brian Aung <soekhantaung16@gmail.com>
2022-12-15 00:22:37 +1100

    error pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,32 @@
-import Link from 'next/link'
+import { Center, Heading, Stack, Text } from '@chakra-ui/react'
+import MyLink from '../components/myLink'
+import utilStyles from '../styles/utils.module.scss'
 
 export default function Custom404() {
   return (
-    <h1>
-      404 Error. Page not found. Go back <Link href="/">home</Link>.
-    </h1>
+    <Center h="100vh">
+      <Stack position="relative" bg="white" border="solid 2px black" boxShadow="2px 2px 0 black" p="3rem" w="60%">
+        <Heading
+          color="primary.500"
+          className={utilStyles.textWithStroke}
+          fontSize="5rem"
+          position="absolute"
+          top="-30%"
+          left="-2%">
+          404
+        </Heading>
+        <Heading>Hello?? Is somebody there?!?</Heading>
+        <Text as="b">
+          You've found a page that doesn't exist. It is scary in here so go back
+          <MyLink onClick={null} href="/">
+            {' '}
+            <Text display="inline" textDecorationLine="underline" textDecorationThickness="2px">
+              home
+            </Text>{' '}
+          </MyLink>
+          as soon as possible.
+        </Text>
+      </Stack>
+    </Center>
   )
 }
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,9 +1,22 @@
-import Link from 'next/link'
+import { Center, Heading, Stack, Text } from '@chakra-ui/react'
+import utilStyles from '../styles/utils.module.scss'
 
 export default function Custom500() {
   return (
-    <h1>
-      500 Error. Go back <Link href="/">home</Link>.
-    </h1>
+    <Center h="100vh">
+      <Stack position="relative" bg="white" border="solid 2px black" boxShadow="2px 2px 0 black" p="3rem" w="60%">
+        <Heading
+          color="primary.500"
+          className={utilStyles.textWithStroke}
+          fontSize="5rem"
+          position="absolute"
+          top="-30%"
+          left="-2%">
+          500
+        </Heading>
+        <Heading>Oops!</Heading>
+        <Text as="b">Something went wrong with the server. Please refresh or try again later.</Text>
+      </Stack>
+    </Center>
   )
 }
